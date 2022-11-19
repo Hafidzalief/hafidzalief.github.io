7c116dcf294d548a8cafc29497ca23c289467054
Hafidzalief <118587330+Hafidzalief@users.noreply.github.com>
2022-11-19 12:13:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var hamburger = document.querySelector(".hamb")
+var navlist = document.queryselector(".nav-list")
+var links = document.queryselector(".nav-list li")
+
+hamburger.addEventListener("click", funciton(){
+	this.classList.toogle{"click"};
+	nsvlidt.classList.toggle("open");
+});
